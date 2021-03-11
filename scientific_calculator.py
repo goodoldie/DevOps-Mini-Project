0c4797de0d57f947e829f6e7d9140f2d68e87cb1
Pawan Gupta <goodoldie1996@gmail.com>
2021-03-11 12:22:28 +0530

    Added event click and mathematical conditions

--- scientific_calculator.py
+++ scientific_calculator.py
@@ -20,6 +20,45 @@ def bkspace():
     length=len(current)-1
     e.delete(length, END)
 
+def evaluate():
+    ans=e.get()
+    ans=eval(ans)
+    e.delete(0, END)
+    e.insert(0, ans)
+
+def sc(event):
+    key=event.widget
+    text=key['text']
+    no=e.get()
+    result=''
+    if text=='deg':
+        result=str(m.degrees(float(no)))
+    if text=='sin':
+        result=str(m.sin(float(no)))
+    if text=='cos':
+        result=str(m.cos(float(no)))
+    if text=='tan':
+        result=str(m.tan(float(no)))
+    if text=='lg':
+        result=str(m.log10(float(no)))
+    if text=='ln':
+        result=str(m.log(float(no)))
+    if text=='Sqrt':
+        result=str(m.sqrt(float(no)))
+    if text=='x!':
+        result=str(m.factorial(float(no)))
+    if text=='1/x':
+        result=str(1/(float(no)))
+    if text=='pi':
+        if no=="":
+            result=str(m.pi)
+        else:
+            result=str(float(no) * m.pi)
+    if text=='e':
+        if no=="":
+            result=str(m.e)
+        else:
+            result=str(m.e**float(no))
 
 
 
