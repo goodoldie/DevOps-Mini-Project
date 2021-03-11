87f1b09f179858d91aa045bd15eed06323724bc4
Pawan Gupta <goodoldie1996@gmail.com>
2021-03-11 20:14:53 +0530

    Added Grid for GUI, Buttons and their functions and Toolbar options

--- scientific_calculator.py
+++ scientific_calculator.py
@@ -173,6 +173,86 @@ class Calc:
 
 res = Calc()
 
+#Grid of GUI
+
+txtDisplay = Entry(calc,font=('arial',20,'bold'),bd=30,bg='powder blue',width=28,justify=RIGHT)
+txtDisplay.grid(row=0,column=0,columnspan=4,pady=1)
+txtDisplay.insert(0,'0')
+
+numpad = '789456123'
+i=0
+btn = []
+
+for j in range(2,5):
+    for k in range(3):
+        btn.append(Button(calc,width=6,height=2,font=('arial',20,'bold'),bd=4,text=numpad[i],bg="gray10"))
+        btn[i].grid(row=j,column=k,pady=1)
+        btn[i]['command'] = lambda x = numpad [i]:res.numberEnter(x)
+        i+=1
+
+#Standard Calculator Functions
+Button(calc,text=chr(67),width=6,height=2,font=('arial',20,'bold'),bd=4,bg="gray40",command = res.clear_entry).grid(row=1,column=0,pady=1)
+Button(calc,text=chr(67)+chr(69),width=6,height=2,font=('arial',20,'bold'),bd=4,bg="gray40",command = res.clear_all).grid(row=1,column=1,pady=1)
+Button(calc,text=u'\u221A',width=6,height=2,font=('arial',20,'bold'),bd=4,bg="gray40",command = res.sq_rt).grid(row=1,column=2,pady=1)
+Button(calc,text='+',width=6,height=2,font=('arial',20,'bold'),bd=4,bg="gray40",command = lambda: res.operation('add')).grid(row=1,column=3,pady=1)
+
+Button(calc,text='-',width=6,height=2,font=('arial',20,'bold'),bd=4,bg="gray40",command = lambda: res.operation('sub')).grid(row=2,column=3,pady=1)
+Button(calc,text='*',width=6,height=2,font=('arial',20,'bold'),bd=4,bg="gray40",command = lambda: res.operation('mul')).grid(row=3,column=3,pady=1)
+Button(calc,text='/',width=6,height=2,font=('arial',20,'bold'),bd=4,bg="gray40",command = lambda: res.operation('div')).grid(row=4,column=3,pady=1)
+Button(calc,text='=',width=6,height=2,font=('arial',20,'bold'),bd=4,bg="gray40",command = res.sum_of_total).grid(row=5,column=3,pady=1)
+
+Button(calc,text='.',width=6,height=2,font=('arial',20,'bold'),bd=4,bg="gray40",command = lambda: res.numberEnter('.')).grid(row=5,column=0,pady=1)
+Button(calc,text='0',width=6,height=2,font=('arial',20,'bold'),bd=4,bg="gray10",command = lambda: res.numberEnter(0)).grid(row=5,column=1,pady=1)
+Button(calc,text=chr(177),width=6,height=2,font=('arial',20,'bold'),bd=4,bg="gray40",command = res.PM).grid(row=5,column=2,pady=1)
+
+#Scientific Calculator Functions
+Button(calc,text=u'\u03C0',width=6,height=2,font=('arial',20,'bold'),bd=4,bg="gray20",command = res.pi).grid(row=1,column=4,pady=1)
+Button(calc,text='sin',width=6,height=2,font=('arial',20,'bold'),bd=4,bg="gray20",command = res.sin).grid(row=2,column=4,pady=1)
+Button(calc,text='cos',width=6,height=2,font=('arial',20,'bold'),bd=4,bg="gray20",command = res.cos).grid(row=2,column=5,pady=1)
+Button(calc,text='tan',width=6,height=2,font=('arial',20,'bold'),bd=4,bg="gray20",command = res.tan).grid(row=2,column=6,pady=1)
+
+Button(calc,text='log',width=6,height=2,font=('arial',20,'bold'),bd=4,bg="gray20",command = res.log).grid(row=4,column=4,pady=1)
+Button(calc,text='sinh',width=6,height=2,font=('arial',20,'bold'),bd=4,bg="gray20",command = res.sinh).grid(row=3,column=4,pady=1)
+Button(calc,text='cosh',width=6,height=2,font=('arial',20,'bold'),bd=4,bg="gray20",command = res.cosh).grid(row=3,column=5,pady=1)
+Button(calc,text='tanh',width=6,height=2,font=('arial',20,'bold'),bd=4,bg="gray20",command = res.tanh).grid(row=3,column=6,pady=1)
+
+Button(calc,text='ln',width=6,height=2,font=('arial',20,'bold'),bd=4,bg="gray20",command = res.ln).grid(row=4,column=5,pady=1)
+Button(calc,text='exp',width=6,height=2,font=('arial',20,'bold'),bd=4,bg="gray20",command = res.exp).grid(row=1,column=6,pady=1)
+Button(calc,text='mod',width=6,height=2,font=('arial',20,'bold'),bd=4,bg="gray20",command = lambda: res.operation('mod')).grid(row=5,column=6,pady=1)
+Button(calc,text='e',width=6,height=2,font=('arial',20,'bold'),bd=4,bg="gray20",command = res.e).grid(row=1,column=5,pady=1)
+
+Button(calc,text='log2',width=6,height=2,font=('arial',20,'bold'),bd=4,bg="gray20",command = res.log2).grid(row=4,column=6,pady=1)
+Button(calc,text='deg',width=6,height=2,font=('arial',20,'bold'),bd=4,bg="gray20",command = res.degrees).grid(row=5,column=4,pady=1)
+Button(calc,text='rad',width=6,height=2,font=('arial',20,'bold'),bd=4,bg="gray20",command = res.radians).grid(row=5,column=5,pady=1)
+
+
+#File Menu in Toolbar
+
+def Exit():
+    if tkinter.messagebox.askyesno("Calculator","Confirm if you want to quit") >0 :
+        root.destroy()
+        return
+
+
+def Sci():
+    root.resizable(width=False,height=False)
+    root.geometry("835x568+0+0")
+
+
+def Std():
+    root.resizable(width=False,height=False)
+    root.geometry("480x568+0+0")
+
+menubar = Menu(calc)
+
+file_menu = Menu(menubar,tearoff=0)
+menubar.add_cascade(label="File",menu=file_menu)
+file_menu.add_command(label="Standard",command=Std)
+file_menu.add_command(label="Scientific",command=Sci)
+file_menu.add_separator()
+file_menu.add_command(label="Exit",command=Exit)
+
+root.config(menu=menubar)
 
 
 root.mainloop()
