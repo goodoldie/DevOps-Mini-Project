a239b6b78678ba89b50ae1810efa9bcab84179bf
Pawan Gupta <goodoldie1996@gmail.com>
2021-03-12 14:18:58 +0530

    Added remaining functions with their test cases

--- scientific_calculator.py
+++ scientific_calculator.py
@@ -8,3 +8,11 @@ def square_root(x):
 
 def factorial(x):
     return m.factorial(x)
+
+
+def natural_log(x):
+    return m.log(x)
+
+
+def power(x, y):
+    return (x ** y)
--- test_scientific_calculator.py
+++ test_scientific_calculator.py
@@ -11,3 +11,9 @@ class TestCalculator:
 
     def test_factorial(self):
         assert 5040 == scientific_calculator.factorial(7)
+
+    def test_natural_log(self):
+        assert 2.995732273553991 == scientific_calculator.natural_log(20)
+
+    def test_power(self):
+        assert 512 == scientific_calculator.power(2, 9)
