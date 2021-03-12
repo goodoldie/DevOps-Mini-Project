02ac8831847afd6973e9be001c94fe7fd64f056f
Pawan Gupta <goodoldie1996@gmail.com>
2021-03-12 13:35:53 +0530

    Successfully ran and added test cases for square root and factorial

---
+++ test_scientific_calculator.py
@@ -0,0 +1,12 @@
+"""
+Unit tests for the calculator library
+"""
+import scientific_calculator
+
+class TestCalculator:
+
+    def test_sqrt(self):
+        assert 14 == scientific_calculator.square_root(196)
+
+    def test_factorial(self):
+        assert 5040 == scientific_calculator.factorial(7)
