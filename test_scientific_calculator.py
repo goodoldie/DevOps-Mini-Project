643b3baa8dc31be915287fe2a71490614588e7bc
Pawan Gupta <goodoldie1996@gmail.com>
2021-03-12 13:57:43 +0530

    Added space according to linter in test file

--- test_scientific_calculator.py
+++ test_scientific_calculator.py
@@ -5,8 +5,10 @@ import scientific_calculator
 
 class TestCalculator:
 
+
     def test_sqrt(self):
         assert 14 == scientific_calculator.square_root(196)
+
 
     def test_factorial(self):
         assert 5040 == scientific_calculator.factorial(7)
