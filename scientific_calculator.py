bcbecc12ddb7c032ad312b9f6dea85561d921baa
Pawan Gupta <goodoldie1996@gmail.com>
2021-03-12 12:53:10 +0530

    Removed some functions to test later in CI pipeline

--- scientific_calculator.py
+++ scientific_calculator.py
@@ -1,14 +1,18 @@
-#Scientific Calculator Functions
+# Scientific Calculator Functions
 import math as m
+
 
 def square_root(x):
     return m.sqrt(x)
 
+
 def factorial(x):
     return m.factorial(x)
 
-def natural_log(x):
-    return m.log(x)
 
-def power(x, y):
-    return (x ** y)
+# def natural_log(x):
+#     return m.log(x)
+#
+#
+# def power(x, y):
+#     return (x ** y)
