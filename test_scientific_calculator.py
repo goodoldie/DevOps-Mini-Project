67a9af524987238743923e501815056549635a04
Pawan Gupta <goodoldie1996@gmail.com>
2021-03-12 14:03:10 +0530

    Adjusted some spacing

--- test_scientific_calculator.py
+++ test_scientific_calculator.py
@@ -3,12 +3,11 @@ Unit tests for the calculator library
 """
 import scientific_calculator
 
-class TestCalculator:
 
+class TestCalculator:
 
     def test_sqrt(self):
         assert 14 == scientific_calculator.square_root(196)
-
 
     def test_factorial(self):
         assert 5040 == scientific_calculator.factorial(7)
